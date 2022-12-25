47fa5ec31bca14a691fae8a7c94f492b62f88c9b
Maxx Borer <maxxborer@gmail.com>
2022-12-25 23:25:45 +0600

    fix: package version and theme type

--- src/styles/theming.ts
+++ src/styles/theming.ts
@@ -1,15 +1,17 @@
 import { createTheming } from "@callstack/react-theme-provider";
 
+const theme = {
+  primaryColor: "red",
+  secondaryColor: "green",
+};
+
+const { ThemeProvider: ThemeProviderBase, withTheme } = createTheming(theme);
+
 type ThemeProviderType<Theme> = React.ComponentType<{
   children: React.ReactNode;
   theme?: Theme;
 }>;
 
-const { ThemeProvider: ThemeProviderBase, withTheme } = createTheming({
-  primaryColor: "red",
-  secondaryColor: "green",
-});
-
-const ThemeProvider = ThemeProviderBase as ThemeProviderType<Theme>;
+const ThemeProvider = ThemeProviderBase as ThemeProviderType<typeof theme>;
 
 export { ThemeProvider, withTheme };
